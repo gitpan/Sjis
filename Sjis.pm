eb07adc717fa16851c06266d76c70b6aeae03ac3
INABA Hitoshi <ina@cpan.org>
2011-01-26 13:39:38 +0000

    Import of INA/Sjis-0.71 from CPAN.
    gitpan-cpan-distribution: Sjis
    gitpan-cpan-version:      0.71
    gitpan-cpan-path:         INA/Sjis/Sjis-0.71.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Sjis.pm
+++ Sjis.pm
@@ -20,7 +20,7 @@ use Esjis;
 
 BEGIN { eval q{ use vars qw($VERSION) } }
 
-$VERSION = sprintf '%d.%02d', q$Revision: 0.70 $ =~ m/(\d+)/oxmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.71 $ =~ m/(\d+)/oxmsg;
 
 # poor Symbol.pm - substitute of real Symbol.pm
 BEGIN {
@@ -39,14 +39,10 @@ BEGIN {
 # in Chapter 29: Functions
 # of ISBN 0-596-00027-8 Programming Perl Third Edition.
 
-unless (eval q{ use Fcntl qw(:flock); 1 }) {
-    eval q{
-        sub LOCK_SH {1}
-        sub LOCK_EX {2}
-        sub LOCK_UN {8}
-        sub LOCK_NB {4}
-    };
-}
+sub LOCK_SH() {1}
+sub LOCK_EX() {2}
+sub LOCK_UN() {8}
+sub LOCK_NB() {4}
 
 # P.707 29.2.33. exec
 # in Chapter 29: Functions
@@ -3376,9 +3372,7 @@ sub e_qr {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
-            }
-            elsif ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
+            if ($char[$i-1] =~ m/\A (?:[\x00-\xFF]|\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -3487,7 +3481,7 @@ sub e_qr_q {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
+            if ($char[$i-1] =~ m/\A [\x00-\xFF] \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -3823,9 +3817,7 @@ sub e_s1 {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
-            }
-            elsif ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
+            if ($char[$i-1] =~ m/\A (?:[\x00-\xFF]|\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -3936,7 +3928,7 @@ sub e_s1_q {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
+            if ($char[$i-1] =~ m/\A [\x00-\xFF] \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -4469,9 +4461,7 @@ sub e_split {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
-            }
-            elsif ($char[$i-1] =~ m/\A (?:\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
+            if ($char[$i-1] =~ m/\A (?:[\x00-\xFF]|\\[0-7]{2,3}|\\x[0-9-A-Fa-f]{1,2}) \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
@@ -4579,7 +4569,7 @@ sub e_split_q {
 
         # quote character before ? + * {
         elsif (($i >= 1) and ($char[$i] =~ m/\A [\?\+\*\{] \z/oxms)) {
-            if (CORE::length($char[$i-1]) == 1) {
+            if ($char[$i-1] =~ m/\A [\x00-\xFF] \z/oxms) {
             }
             else {
                 $char[$i-1] = '(?:' . $char[$i-1] . ')';
